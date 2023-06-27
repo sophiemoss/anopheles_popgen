0d672634595e7a4d1ff9e417fff2b7da72eddbbf
Sophie Moss <sophie.moss04@gmail.com>
2023-06-27 13:32:58 +0000

    renamed check_fastq.sh and modified runfastq2vcf.sh

--- runfastq2vcf.sh
+++ runfastq2vcf.sh
@@ -4,7 +4,7 @@
 
 ls *_1.fastq.gz | sed 's/_1.fastq.gz//' > fastq2vcfsamples.txt
 
-cat fastq2vcfsamples.txt | parallel -j 45 \
+cat fastq2vcfsamples.txt | parallel -j 15 \
 "fastq2vcf.py all --read1 {}_1.fastq.gz --read2 {}_2.fastq.gz \
 --ref Anopheles_gambiae.AgamP4.dna.toplevel.fa \
 --prefix {}" > fastq2vcf_log.txt 2>&1
