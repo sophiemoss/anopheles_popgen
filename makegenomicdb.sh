638ec9f5337a2b208aac94812d0beebc6bb51bf2
Sophie Moss <sophie.moss04@gmail.com>
2023-07-04 15:08:31 +0000

    changes to fastq combining python script

---
+++ makegenomicdb.sh
@@ -0,0 +1,4 @@
+## to make a genomics database of sample VCFs, use the following
+
+/mnt/storage11/sophie/fastq2matrix/scripts/merge_vcfs.py import --sample-file fastq2vcfsamples.txt --ref /mnt/storage11/sophie/reference_genomes/A_gam_P4_ensembl/Anopheles_gambiae.AgamP4.dna.toplevel.fa --prefix gambiae_nov2022 --vcf-dir .
+
