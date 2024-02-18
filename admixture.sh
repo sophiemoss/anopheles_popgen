b3b16bbb90fb46c44c7e73897fe65807d3feae9e
Sophie Moss <sophie.moss04@gmail.com>
2024-02-18 21:19:50 +0000

    admixture

--- admixture.sh
+++ admixture.sh
@@ -10,11 +10,11 @@ bcftools query -l yourfile.vcf.gz
 
 zgrep -v "^#" FMISSING_MAF_AC0_DP5_GQ20_gatk_filtered_miss_40_mac_bi_snps_melas_2019_plusglobal.2023_07_25.genotyped.ann.vcf.gz | cut -f1 | sort | uniq
 
-zcat FMISSING_MAF_AC0_DP5_GQ20_gatk_filtered_miss_40_mac_bi_snps_melas_2019_plusglobal.2023_07_25.genotyped.ann.vcf.gz | awk 'BEGIN{OFS=FS="\t"} /^#/ {print; next} {gsub(/^2L$/, "1", $1); gsub(/^2R$/, "2", $1); gsub(/^3L$/, "3", $1); gsub(/^3R$/, "4", $1); gsub(/^Mt$/, "5", $1); gsub(/^X$/, "6", $1); gsub(/^Y_unplaced$/, "7", $1); print}' | gzip > modified.vcf.gz
+zcat FMISSING_MAF_AC0_DP5_GQ20_gatk_filtered_miss_40_mac_bi_snps_melas_2019_plusglobal.2023_07_25.genotyped.ann.vcf.gz | awk 'BEGIN{OFS=FS="\t"} /^#/ {print; next} {gsub(/^2L$/, "1", $1); gsub(/^2R$/, "2", $1); gsub(/^3L$/, "3", $1); gsub(/^3R$/, "4", $1); gsub(/^Mt$/, "5", $1); gsub(/^X$/, "6", $1); gsub(/^Y_unplaced$/, "7", $1); print}' | gzip > gambiae_malariagen_GB_GM-ABC_Bijagos_merged_renamedchr.vcf.gz
 
 # STEP 3 MAKE BED AND BIM FILES
 
-plink --vcf modified.vcf.gz --set-missing-var-ids @:# --keep-allele-order --const-fid --allow-extra-chr --make-bed --out gambiae_global_gambiaealigned
+plink --vcf gambiae_malariagen_GB_GM-ABC_Bijagos_merged_renamedchr.vcf.gz --set-missing-var-ids @:# --keep-allele-order --const-fid --allow-extra-chr --make-bed --out gambiae_global_gambiaealigned
 
 # STEP 4 RUN ADMIXTURE
 
